bcfc345798bc9643a1a5fc730a6e2db13ea93bec
wellhope <710205009@qq.com>
2018-08-29 16:15:22 +0800

    修改配置

--- deploy_jdk.sh
+++ deploy_jdk.sh
@@ -6,16 +6,20 @@ mvn clean install package
 
 ### 运行
 cd $PROJ_PATH/mycloud/mycloud-eureka/target
-java -jar mycloud-eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro
+java -jar mycloud-eureka-0.0.1-SNAPSHOT.jar &
 
 cd $PROJ_PATH/mycloud/mycloud-static/target
-java -jar mycloud-static-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro
+java -jar mycloud-static-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro &
+
+sleep 5
 
 cd $PROJ_PATH/mycloud/mycloud-sso/mycloud-sso-server/target
-java -jar mycloud-sso-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro
+java -jar mycloud-sso-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro &
+
+sleep 10
 
 cd $PROJ_PATH/mycloud/mycloud-movie/mycloud-movie-admin/target
-java -jar mycloud-movie-admin-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro
+java -jar mycloud-movie-admin-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro &
 
 cd $PROJ_PATH/mycloud/mycloud-movie/mycloud-movie-client/target
-java -jar mycloud-movie-client-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro
+java -jar mycloud-movie-client-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro &
